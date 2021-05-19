1e7a8c601b0b1463768dd4dba6f32a12bd60c44d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-19 21:12:28 +0200

    Agrego el parámetro .data al return

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -4,7 +4,7 @@ class Pelicula{
 
     async findTopRated(){
         let res = await axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US&page=1");
-        return res;
+        return res.data;
     }
 }
 
