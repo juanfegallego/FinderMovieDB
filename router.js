56ad750dd2363f0d1d6bf95ced340f70a67be686
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-19 23:25:55 +0200

    Ruta para buscar las series creada y vinculada

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
 const moviesRouter = require("./routers/moviesRouter");
+const seriesRouter = require("./routes/seriesRouter");
 
 
 router.use("/movies", moviesRouter);
+router.use("/series", seriesRouter);
 
 module.exports = router;
 
