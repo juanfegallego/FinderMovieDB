b13360143b9b1289894e84734b68f9970a5e53c9
juanfegallego <juanfegallegodj@gmail.com>
2021-05-19 20:49:08 +0200

    cambio de ruta movie a movies

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const moviesRouter = require("./routers/moviesRouter");
 
 
-router.use("/movie", moviesRouter);
+router.use("/movies", moviesRouter);
 
 module.exports = router;
 
