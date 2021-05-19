59770d61ce0f7693e13294dd40b1750ce2ab13ab
juanfegallego <juanfegallegodj@gmail.com>
2021-05-19 23:53:56 +0200

    Creados y testados los endpoints de Series

---
+++ controllers/seriesControllers.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+class Series{
+
+    async findTopRated(){
+        let res = await axios.get("https://api.themoviedb.org/3/tv/top_rated?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US&page=1");
+        return res.data;
+    }
+
+    async searchById(id){
+        let res = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US`);
+        return res.data;
+    }
+
+    async searchByTitle(query){1
+        let res = await axios.get(`https://api.themoviedb.org/3/search/tv?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&query=${query}`);
+        return res.data;
+    }
+}
+
+
+let seriesController = new Series();
+module.exports = seriesController;
