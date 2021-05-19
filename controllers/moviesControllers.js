0c29ad6bfe14767ff31abbb2cbe3fa072e4ad994
juanfegallego <juanfegallegodj@gmail.com>
2021-05-20 01:32:31 +0200

    Endpoint de busqueda por genero

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -16,6 +16,11 @@ class Pelicula{
         let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&query=${query}`);
         return res.data;
     }
+
+    async findToGenre(){1
+        let res = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US`);
+        return res.data;
+    }
 }
 
 
