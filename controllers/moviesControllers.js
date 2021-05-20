ccc40484da5ab70878784afb5c97166c822aa064
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-20 18:18:12 +0200

    modificamos ruta de enlace para buscar por géneros y testeamos con resultado 200

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -12,104 +12,25 @@ class Pelicula{
         return res.data;
     }
 
-    async searchByTitle(query){1
+    async searchByTitle(query){
         let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&query=${query}`);
         return res.data;
     }
 
-    async findToGenre(name){1
-        let res = await axios.get(`https://api.themoviedb.org/3/genre/movie/${name}?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US`);
-        return res.data;
+    async findToGenre(name){
+
+        let array1 = await axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US");
+        let newArray = array1.data.genres;
+
+        for (let i = 0; i < newArray.length; i++) {
+            if (newArray[i].name == name) {
+                let id = newArray[i].id;
+                let res = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&with_genres=${id}`);
+                return res.data;
+            }
+        }
     }
 }
-
-
-    //Traductor de Generos
-    const genre = [
-        {
-            "id": 28,
-            "name": "Action"
-        },
-        {
-            "id": 12,
-            "name": "Adventure"
-        },
-        {
-            "id": 16,
-            "name": "Animation"
-        },
-        {
-            "id": 35,
-            "name": "Comedy"
-        },
-        {
-            "id": 80,
-            "name": "Crime"
-        },
-        {
-            "id": 99,
-            "name": "Documentary"
-        },
-        {
-            "id": 18,
-            "name": "Drama"
-        },
-        {
-            "id": 10751,
-            "name": "Family"
-        },
-        {
-            "id": 14,
-            "name": "Fantasy"
-        },
-        {
-            "id": 36,
-            "name": "History"
-        },
-        {
-            "id": 27,
-            "name": "Horror"
-        },
-        {
-            "id": 10402,
-            "name": "Music"
-        },
-        {
-            "id": 9648,
-            "name": "Mystery"
-        },
-        {
-            "id": 10749,
-            "name": "Romance"
-        },
-        {
-            "id": 878,
-            "name": "Science Fiction"
-        },
-        {
-            "id": 10770,
-            "name": "TV Movie"
-        },
-        {
-            "id": 53,
-            "name": "Thriller"
-        },
-        {
-            "id": 10752,
-            "name": "War"
-        },
-        {
-            "id": 37,
-            "name": "Western"
-        }
-    ]
-
-
-
-    for(let i = 0; i < genre.length; ++i ) {
-            console.log(`${genre[i].name}`)
-            // console.log( `${carrito[i].nombre} - Precio: ${carrito[i].precio }` );//Para acceder al precio
-    }
 
 let moviesController = new Pelicula();
 module.exports = moviesController;
