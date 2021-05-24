7482e38567a3e63b43aaec6e2ee4c5e7829f686c
juanfegallego <juanfegallegodj@gmail.com>
2021-05-24 18:23:32 +0200

    archivo user.js creado

---
+++ models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    nombre: DataTypes.STRING,
+    apellido1: DataTypes.STRING,
+    apellido2: DataTypes.STRING,
+    edad: DataTypes.INTEGER,
+    direccion: DataTypes.STRING,
+    cp: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    telefono: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
