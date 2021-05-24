8af82cfd96bd5fd8392d64fd12c6f91727d12856
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-24 22:24:22 +0200

    Corrijo endpoint createUser && agrego endpoint findAllUsers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,11 +3,22 @@ const usersControllers = require("../controllers/usersControllers");
 
 router.post('/',async (req, res) => {
     try {
-        const user = req.body;
-        res.json(await usersControllers.createUser(user));
+        const id = await usersControllers.createUser(req.body);
+        const status = "success";
+        res.json({ status, id });
     }catch (err) {
         return res.status(500).json({
             message: err.message
+        });
+    }
+});
+
+router.get("/", async(req, res) => {
+    try {
+        res.json(await usersControllers.findAllUsers());
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
         });
     }
 });
