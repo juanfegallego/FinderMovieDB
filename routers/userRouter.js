a2513894c029ee598d265e30849c3ee14e7ef84f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-25 21:57:34 +0200

    Creo ruta para modificar y findUser por id

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -13,6 +13,28 @@ router.post('/',async (req, res) => {
     }
 });
 
+router.get("/:id", async(req, res) => {
+    try {
+        const id = req.params.id;
+        res.json(await usersControllers.userId(id));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+router.put("/", async(req, res) => {
+    try {
+        const body = req.body;
+        res.json( await usersControllers.modifyUser(body));
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
 router.get("/", async(req, res) => {
     try {
         res.json(await usersControllers.findAllUsers());
