2d7195b1ad3bb68ddbcd5cd791216d20ca4e3193
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-26 19:07:35 +0200

    Corrigo path de login && nombre del metodo nameUse

--- routers/login.router.js
+++ routers/login.router.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const loginController = require("../controllers/login.controller");
 const userController = require("../controllers/usersControllers");
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         const nombreCheck = req.body.nombre;
         const passwordCheck = req.body.password;
         let token = await loginController.validate( nombreCheck, passwordCheck );
-        let user = await userController.nameUser( nombreCheck );
+        let user = await userController.nameUse( nombreCheck );
         res.status(200).json( {token, user})
     } catch (error) {
         return res.status(500).json({
