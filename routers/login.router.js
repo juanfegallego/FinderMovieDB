2d3ab2b9770a1be638e11527f1bd72965c820d7a
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-26 16:51:53 +0200

    Creo el post de login

---
+++ routers/login.router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const loginController = require("../controllers/login.controller");
+const userController = require("../controllers/usersControllers");
+
+router.post("/login", async (req, res) => {
+    try {
+        const nombreCheck = req.body.nombre;
+        const passwordCheck = req.body.password;
+        let token = await loginController.validate( nombreCheck, passwordCheck );
+        let user = await userController.nameUser( nombreCheck );
+        res.status(200).json( {token, user})
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+});
