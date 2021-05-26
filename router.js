47db0d7e964279b705ea5a1b6448061b7c264336
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-26 16:51:01 +0200

    Genero el path para login

--- router.js
+++ router.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const moviesRouter = require("./routers/moviesRouter");
 const seriesRouter = require("./routers/seriesRouter");
 const userRouter = require("./routers/userRouter");
+const loginRouter = require("./routers/login.router");
 
 router.use("/movies", moviesRouter);
 router.use("/series", seriesRouter);
 router.use("/users", userRouter);
+router.use("/login", loginRouter);
 
 module.exports = router;
 
