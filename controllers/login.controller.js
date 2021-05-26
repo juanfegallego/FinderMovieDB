d4d6740d8d209b616110c83bb2575a1ef5ec9356
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-26 17:19:57 +0200

    Login.controller creado y enlazado conLogin.router

---
+++ controllers/login.controller.js
@@ -0,0 +1,31 @@
+const userController = require("./usersControllers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secret = "Somos un equipazo";
+
+
+class LoginController {
+
+    async validate( nombreCheck, passwordCheck ) {
+
+        let user = await userController.nameUse(nombreCheck);
+
+        let password = user.password;
+
+        let verificar = await bcrypt.compare(passwordCheck, password);
+
+        if(!verificar) {
+            return new Error("El password y el email no coinciden");
+        }
+
+        let payload = {
+            userId: user.id,
+            createdAt: new Date,
+        };
+
+        return jwt.sign(payload, secret);
+    }
+}
+
+let loginController = new LoginController();
+module.exports = loginController;
