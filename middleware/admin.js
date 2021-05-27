b92d59b1898a7d704d447804b5ba4b3efd619c4f
juanfegallego <juanfegallegodj@gmail.com>
2021-05-27 18:49:03 +0200

    Mw de admin creado

---
+++ middleware/admin.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const secret = "Somos un equipazo";
+
+const admin = (req,res,next) => {
+    try {
+
+        if(!req.headers.authorization){
+            // return new Error("No tienes autorización");
+            return "no tenías token ";
+            
+        }
+
+        let token = req.headers.authorization.split(' ')[1];
+
+        let auth = jwt.verify(token,secret);
+
+        if((auth.userId != req.body.id) && (auth.isAdmin == req.body.isAdmin)){
+            throw new Error("No tienes permiso para realizar esta acción");
+        }
+
+        return next();
+
+    } catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = admin;
