4e5efe48ebb29767e32d8ee8e44c202dcfdb63a6
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-27 12:56:16 +0200

    Creo ruta login y instancio suRuta hacia laCarpeta

--- router.js
+++ router.js
@@ -4,11 +4,13 @@ const moviesRouter = require("./routers/moviesRouter");
 const seriesRouter = require("./routers/seriesRouter");
 const userRouter = require("./routers/userRouter");
 const loginRouter = require("./routers/login.router");
+const orderRouter = require("./routers/orders.router");
 
+router.use("/login", loginRouter);
 router.use("/movies", moviesRouter);
+router.use("/orders", orderRouter);
 router.use("/series", seriesRouter);
 router.use("/users", userRouter);
-router.use("/login", loginRouter);
 
 module.exports = router;
 
