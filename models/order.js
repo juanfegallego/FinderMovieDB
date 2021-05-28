82e0094d492b76673f29222e020e8c749648caad
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-05-28 11:02:04 +0200

    Agrego belongsTo para orders y users

--- models/order.js
+++ models/order.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Order.belongsTo(models.User, { foreignKey: "userId" });
     }
   };
   Order.init({
